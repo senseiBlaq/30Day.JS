c102f365fa25ded3d424c4e238960a6217c0f36e
sensei_blaq <jnr.amamoo@gmail.com>
2023-08-04 08:35:13 +0100

    link the porjects to the grid in the script file

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function display_none () { // hides the hero section
     container.setAttribute('id', 'none');
 }
 
-function create_div () { // creates a numbered grid with lins to the projects
+function create_div () { // creates a numbered grid with lins to the projects and links to the projects
     const container = document.querySelector('#grid--container');
 
     for (let i = 0; i < 30; i++) {
         const div = document.createElement('div');
         div.classList.add('grid--items');
         div.classList.add('hover');
-        div.innerHTML = `<a href = 'google.com' > ${i + 1} </a>`;
+        div.innerHTML = `<a href = './projects/project${i+1}.html' > ${i + 1} </a>`; // links to the projects
         container.appendChild(div);
     }
 }
